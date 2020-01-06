80c14d40039eae026235ebe243768696bd263c15
Xingchao Long <z15881614642@163.com>
2020-01-07 01:32:04 +0800

    maven第三章项目

---
+++ helloworld/src/main/java/com/fhlxc/mvnbook/helloworld/App.java
@@ -0,0 +1,13 @@
+package com.fhlxc.mvnbook.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
